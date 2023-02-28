14d32f16a9ef084b4cd0538345ac7c502d04c286
Stalbar <aleksey.smorshyok@gmail.com>
2023-02-28 20:25:06 +0300

    add pages and routing

---
+++ lib/pages/qr_generator.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class QRGeneratorScreen extends StatefulWidget {
+  const QRGeneratorScreen({super.key});
+
+  @override
+  State<QRGeneratorScreen> createState() => _QRGeneratorScreenState();
+}
+
+class _QRGeneratorScreenState extends State<QRGeneratorScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
